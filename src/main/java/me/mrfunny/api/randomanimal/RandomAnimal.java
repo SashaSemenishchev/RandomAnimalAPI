285649b5eccbc9785ca01876f68499ea9395cdc4
Sasha Semenishchev <sashasemenishchev@gmail.com>
2020-11-11 16:23:18 +0300

    Critical fix

--- src/main/java/me/mrfunny/api/randomanimal/RandomAnimal.java
+++ src/main/java/me/mrfunny/api/randomanimal/RandomAnimal.java
@@ -1,22 +1,27 @@
 package me.mrfunny.api.randomanimal;
 
-import com.google.gson.Gson;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RandomAnimal {
 
     public static final String google = "https://www.google.com/search?tbm=isch&q=";
     public static final String charset = "UTF-8";
+    public static final String userAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
 
     private static final Random random = new Random();
 
-    public static String getRandomAnimalImage(AnimalType type) throws IOException {
+    public static String getRandomAnimalImage(AnimalType type) {
         String search = "";
         switch (type){
             case CAT:
@@ -30,15 +35,27 @@ public class RandomAnimal {
                 break;
         }
 
-        GoogleResults searchData = getSearch(search);
+        List<String> links = getSearch(search);
 
-        return searchData.getResponseData().getResults().get(random.nextInt(searchData.getResponseData().getResults().size() - 1)).getUrl();
+        return links.get(random.nextInt(links.size() - 1));
     }
 
-    public static GoogleResults getSearch(String search) throws IOException {
-        System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0");
-        URL url = new URL(google + URLEncoder.encode(search, charset));
-        Reader reader = new InputStreamReader(url.openStream(), charset);
-        return new Gson().fromJson( reader, GoogleResults.class);
+    public static List<String> getSearch(String search){
+        List<String> resultUrls = new ArrayList<>();
+
+        try {
+            Document doc = Jsoup.connect("http://results.dogpile.com/serp?qc=images&q=" + search).userAgent(userAgent).get();
+
+            Elements elements = doc.select(".image a.link");
+
+            for (Element element : elements) {
+                resultUrls.add(element.attr("href"));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return resultUrls;
     }
 }
